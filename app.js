80f77e500e8ec478036b1081eec0d993ff5d6b32
arrC <naveen@arrc.in>
2017-12-24 09:00:43 +0400

    basic code cleanup

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 "use strict"
 require("dotenv").config({path: ".env"})
 
-const express          = require("express")
-const app              = express()
+const express = require("express")
+const app = express()
 const server = require("http").createServer(app)
 const io = require("socket.io")(server)
-const path             = require("path")
-const consolidate      = require("consolidate")
-const swig             = require("swig")
-const bodyParser       = require("body-parser")
+const path = require("path")
+const consolidate = require("consolidate")
+const swig = require("swig")
+const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 const shortid = require('shortid');
-
+const _PORT = 4000;
+// Connection setup
 mongoose.Promise = require("bluebird")
 require("./server/mongoose.js")
 
+// Data model
 const messageSchema = mongoose.Schema({
 	message: { type: String, required: true},
 	username: { type: String },
 	createdAt: { type: Date, default: Date.now}
 });
-
 const Message = mongoose.model('Message', messageSchema);
 
+// Express config
 app.set("showStackError", true)
 app.engine("html", consolidate.swig)
 app.set("view engine", "html")
@@ -30,26 +32,32 @@ app.set("views", path.resolve("server/views"))
 app.use('/static', express.static(__dirname + '/dist'));
 app.use('/vendor', express.static(__dirname + '/client/vendor'))
 
+// Home route
 app.get('/', (req, res, next) => {
 	res.render('index', { title: ">>> Shoutbox <<<" })
 })
 
-server.listen(4000, () => {
-	console.log('listing on port *:4000')
+// Server
+server.listen(_PORT, () => {
+	console.log('listing on port *:_PORT')
 })
 
+// Socket stuff!
 io.on('connection', (client) => {
 	console.log('Client connected...');
 	client['username'] = shortid.generate();
 
+    // Retrive old messages
 	Message.find({}).sort('createdAt').limit(30).exec((err, docs) => {
 		if(err) return client.emit('server:error', { code: 400, message: 'Server error.' })
 		client.emit('server:msgHistory', docs);
 	});
 
+    // Message from client
 	client.on('client:msg', function(data) {
 		Message.create({ message: data, username: client.username }, function(err, messageDoc){
-			if (err) return client.emit('server:error', { code: 400, message: 'message not sent, please try again.' })
+            if (err) return client.emit('server:error', { code: 400, message: 'message not sent, please try again.' })
+            // Broadcast message to all connected client
 			io.emit('server:msg', messageDoc);
 		})
 	})
--- client/js/main.js
+++ client/js/main.js
@@ -1,30 +1,34 @@
 $( () => {
+    const $chatMsgInput = $("#ChatMsgInput");
+    const $chatList = $("#ChatList");
+    const $sendMsgBtn = $("#SendMsgBtn");
     const socket = io.connect('http://localhost:4000');
     
+    // Connection established
     socket.on('connect', () => {
-        socket.emit('client:join', 'Hello World from client');
+        socket.emit('client:join', "I'm in...");
     });
 
+    // On new server message
     socket.on('server:msg', (data) => {
-        console.log("server:msg", data)
         messageTemplate(data)
         document.getElementById('ChatList').lastChild.scrollIntoView(false)
     })
 
+    // Old messages
     socket.on('server:msgHistory', (data) => {
         data.forEach(message => {
             messageTemplate(message)
         })
     })
 
+    // Errors :(
     socket.on('server:error', (data) =>  {
         console.log(data)
+        // Handle error here
     })
 
-    const $chatMsgInput = $("#ChatMsgInput");
-    const $chatList = $("#ChatList");
-    const $sendMsgBtn = $("#SendMsgBtn");
-      
+    // Send on enter
     $chatMsgInput.keydown( (e) => {
         let code = (e.keyCode ? e.keyCode : e.which);
         if (e.keyCode == 13 && !e.shiftKey) {
@@ -33,13 +37,14 @@ $( () => {
         }
     });
 
+    // Transmit the message to the server
     $sendMsgBtn.click( (e) => {
         e.preventDefault();
-        console.log($chatMsgInput.val())
         socket.emit('client:msg', $chatMsgInput.val());
         $chatMsgInput.val('');
     })
 
+    // Message markup, dirty stuff
     function messageTemplate(msg) {
         let _date = moment(msg.createdAt);
         let formatedDate = _date.format("DD/MM/YYYY")
